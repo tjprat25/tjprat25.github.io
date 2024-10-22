9f90ffd475f06cd25d4557cef02debc257928c99
Timothy Pratt <tjprat25@colby.edu>
2024-10-21 20:22:18 -0400

    Added some buttons for convinience. Should be final commit.

--- script.js
+++ script.js
@@ -15,3 +15,23 @@ document.addEventListener('scroll', () => {
             }
     });
 });
+
+document.getElementById("buttonA").addEventListener("click", function() {
+    document.getElementById("headerA").scrollIntoView({behavior: 'smooth'});
+});
+
+document.getElementById("buttonE").addEventListener("click", function() {
+    document.getElementById("headerE").scrollIntoView({behavior: 'smooth'});
+});
+
+document.getElementById("buttonP").addEventListener("click", function() {
+    document.getElementById("headerP").scrollIntoView({behavior: 'smooth'});
+});
+
+document.getElementById("buttonC").addEventListener("click", function() {
+    document.getElementById("headerC").scrollIntoView({behavior: 'smooth'});
+});
+
+document.getElementById("button-top").addEventListener("click", function() {
+    document.getElementById("header-top").scrollIntoView({behavior: 'smooth'});
+});
